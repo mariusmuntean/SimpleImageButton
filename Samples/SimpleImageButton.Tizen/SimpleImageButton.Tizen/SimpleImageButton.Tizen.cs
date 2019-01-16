b1a43a8fc0200fb4bcf33aa215aa86f47bc1d5b0
Marius M <marius.muntean@live.com>
2019-01-16 23:45:17 +0100

    Added a Tizen sample project

---
+++ Samples/SimpleImageButton.Tizen/SimpleImageButton.Tizen/SimpleImageButton.Tizen.cs
@@ -0,0 +1,22 @@
+using System;
+using SimpleImageButton;
+
+namespace SimpleImageButton.Tizen
+{
+    class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+
+            LoadApplication(new Sample.App());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            app.Run(args);
+        }
+    }
+}
